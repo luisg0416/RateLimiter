0a9b4912158f4cc5c7cc1353c6d17a12c0277c96
luisg0416 <lhguerrero0416@gmail.com>
2026-04-29 23:07:21 -0400

    add node types in tsconfig.app.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { copyFileSync } from 'fs';
 
 export default defineConfig({
   plugins: [
     react(),
+    {
+      name: 'copy-staticwebapp-config',
+      closeBundle() {
+        copyFileSync(
+          'staticwebapp.config.json',
+          'dist/staticwebapp.config.json'
+        )
+      }
+    }
   ],
 
   server: {
